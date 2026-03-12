7c8fc5fbdd06ba58bceb5121b1759e8bf7515f19
DevJoshua <joshuabenjamin.jbca@gmail.com>
2026-03-12 12:52:37 -0600

    Actualizacion Profesor

--- info_escolar/src/main/java/mx/uam/controller/ProfesorController.java
+++ info_escolar/src/main/java/mx/uam/controller/ProfesorController.java
@@ -1,46 +1,43 @@
 package mx.uam.controller;
 
-import mx.uam.dto.ProfesorDTO;
-import mx.uam.service.ProfesorService;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/profesores")
 public class ProfesorController {
 
-    private final ProfesorService profesorService;
+    // private final ProfesorService profesorService;
 
-    public ProfesorController(ProfesorService profesorService){this.profesorService = profesorService;}
+    // public ProfesorController(ProfesorService profesorService){this.profesorService = profesorService;}
 
-    //Create
-    @PostMapping
-    public ProfesorDTO createAlumno(@RequestBody ProfesorDTO profesorDTO) {
-        return profesorService.createProfesor(profesorDTO);
-    }
+    // //Create
+    // @PostMapping
+    // public ProfesorDTO createAlumno(@RequestBody ProfesorDTO profesorDTO) {
+    //     return profesorService.createProfesor(profesorDTO);
+    // }
 
-    // GET ALL
-    @GetMapping
-    public List<ProfesorDTO> getProfesor() {
-        return profesorService.getProfesor();
-    }
+    // // GET ALL
+    // @GetMapping
+    // public List<ProfesorDTO> getProfesor() {
+    //     return profesorService.getProfesor();
+    // }
 
-    // GET BY ID
-    @GetMapping("/{id}")
-    public ProfesorDTO getProfesorByID(@PathVariable Long id) {
-        return profesorService.getProfesorById(id);
-    }
-    // UPDATE
-    @PatchMapping("/{id}")
-    public ProfesorDTO updateProfesor(@PathVariable Long id, @RequestBody ProfesorDTO profesorDTO) {
-        return profesorService.updateProfesor(id, profesorDTO);
-    }
-    // DELETE
-    @DeleteMapping("/{id}")
-    public void deleteProfesor(@PathVariable Long id) {
-        profesorService.deleteProfesor(id);
-    }
+    // // GET BY ID
+    // @GetMapping("/{id}")
+    // public ProfesorDTO getProfesorByID(@PathVariable Long id) {
+    //     return profesorService.getProfesorById(id);
+    // }
+    // // UPDATE
+    // @PatchMapping("/{id}")
+    // public ProfesorDTO updateProfesor(@PathVariable Long id, @RequestBody ProfesorDTO profesorDTO) {
+    //     return profesorService.updateProfesor(id, profesorDTO);
+    // }
+    // // DELETE
+    // @DeleteMapping("/{id}")
+    // public void deleteProfesor(@PathVariable Long id) {
+    //     profesorService.deleteProfesor(id);
+    // }
 
 }
 
---
+++ info_escolar/src/main/java/mx/uam/service/ProfesorService.java
@@ -0,0 +1,17 @@
+package mx.uam.service;
+
+import java.util.List;
+
+import mx.uam.dto.ProfesorDTO;
+
+public interface ProfesorService {
+    // ProfesorDTO createProfesor(ProfesorDTO profesorDTO);
+
+    // List<ProfesorDTO> getProfesor();
+
+    // ProfesorDTO getProfesorById(Long id);
+
+    // void deleteProfesor(Long id);
+
+    // ProfesorDTO updateProfesor(Long id, ProfesorDTO profesorDTO);
+}
