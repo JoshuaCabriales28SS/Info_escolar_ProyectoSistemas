9230db9d2cd71e04951e4d2ab679498805665eb4
DevJoshua <joshuabenjamin.jbca@gmail.com>
2026-03-18 10:00:38 -0600

    Creacion de archivos Web de Joshua

--- info_escolar/target/generated-sources/annotations/mx/uam/mapper/AlumnoMapperImpl.java
+++ info_escolar/target/generated-sources/annotations/mx/uam/mapper/AlumnoMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-03-14T18:32:48-0600",
+    date = "2026-03-17T12:16:21-0600",
     comments = "version: 1.5.3.Final, compiler: Eclipse JDT (IDE) 3.45.0.v20260224-0835, environment: Java 21.0.10 (Eclipse Adoptium)"
 )
 @Component
