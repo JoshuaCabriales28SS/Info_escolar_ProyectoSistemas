6e8474bf5a9f9918eb3495e042e9d28eab50055b
DevJoshua <joshuabenjamin.jbca@gmail.com>
2026-03-31 12:20:02 -0600

    actualizacion

--- info_escolar/target/generated-sources/annotations/mx/uam/mapper/TutorMapperImpl.java
+++ info_escolar/target/generated-sources/annotations/mx/uam/mapper/TutorMapperImpl.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-03-30T20:12:54-0600",
+    date = "2026-03-30T20:54:40-0600",
     comments = "version: 1.5.3.Final, compiler: Eclipse JDT (IDE) 3.45.0.v20260224-0835, environment: Java 21.0.10 (Eclipse Adoptium)"
 )
 @Component
